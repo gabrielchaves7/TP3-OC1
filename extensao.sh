bf83a83445c42465884ac8e74ee7e4971ab00c45
Gabriel Chaves <gabriel.chaves@thoughtworks.com>
2021-09-06 09:15:39 -0300

    alterando loop no extensao.sh

--- extensao.sh
+++ extensao.sh
@@ -1,7 +1,7 @@
 EXEC=$1
 
 echo "Executando testes:"
-for i in {1..5..1}; do
+for i in 1 2 3 4 5; do
   testname=$(printf "%02d" $i)
   $EXEC tests/$testname.in result.$testname
   if ! diff -qwB tests/$testname.out result.$testname &>/dev/null; then
